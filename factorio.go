c561915c4f785e0bec16477d5d25b6153e714a79
Peter Czibik <p.czibik@gmail.com>
2018-02-01 00:01:48 +0100

    serialize and deserialize packets

--- factorio.go
+++ factorio.go
@@ -1,26 +1,74 @@
 package main
 
 import (
-        "encoding/binary"
-        "bytes"
+	"encoding/binary"
+	"errors"
+	"fmt"
 	"net"
+	"strings"
 )
 
+const terminationSequence = "\x00"
+
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:2020")
+	conn, err := net.Dial("tcp", "127.0.0.1:27015")
 	if err != nil {
 		panic(err)
 	}
 	defer conn.Close()
 
-        message := "hellok\n"
-        var message_bytes [20]byte
-        message_bytes_slice := message_bytes[:]
-        copy(message_bytes_slice, message)
+	packet := NewPacket(SERVERDATA_AUTH, "hello")
 
-        buffer := bytes.NewBuffer(make([]byte, 0, 14))
-        buffer.WriteString(message)
-        binary.Write(buffer, binary.LittleEndian, message_bytes_slice)
+	err = writePacket(&conn, packet)
+	if err != nil {
+		panic(err)
+	}
+	packet, err = readPacket(&conn)
+	if err != nil {
+		panic(err)
+	}
 
-        conn.Write(buffer.Bytes())
+	fmt.Printf("%+v\n", packet)
+}
+
+func writePacket(conn *net.Conn, packet *Packet) error {
+	buffer := packet.ToBuffer()
+
+	bytes := buffer.Bytes()
+
+	n, err := (*conn).Write(bytes)
+	if n != len(bytes) {
+		return errors.New("Invalid read")
+	}
+	return err
+}
+
+func readPacket(conn *net.Conn) (response *Packet, err error) {
+	var packet Packet
+	if err = binary.Read(*conn, binary.LittleEndian, &packet.Size); err != nil {
+		return
+	}
+	if err = binary.Read(*conn, binary.LittleEndian, &packet.Id); err != nil {
+		return
+	}
+	if err = binary.Read(*conn, binary.LittleEndian, &packet.Type); err != nil {
+		return
+	}
+
+	var n int
+	bytesRead := 0
+	bytesTotal := int(packet.Size - 4)
+	body := make([]byte, bytesTotal)
+
+	for bytesTotal > bytesRead {
+		n, err = (*conn).Read(body[bytesRead:])
+		if err != nil {
+			return
+		}
+		bytesRead += n
+	}
+
+	packet.Body = strings.TrimRight(string(body), terminationSequence)
+
+	return &packet, nil
 }
