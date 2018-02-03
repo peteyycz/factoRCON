8c6367e9984dc730d0483b7234d423381cca347f
Peter Czibik <p.czibik@gmail.com>
2018-02-03 14:17:16 +0100

    add cli version

--- factorio.go
+++ factorio.go
@@ -1,74 +1,43 @@
 package main
 
 import (
-	"encoding/binary"
-	"errors"
+	"bufio"
 	"fmt"
-	"net"
+	"os"
 	"strings"
+
+	rcon "github.com/gtaylor/factorio-rcon"
 )
 
-const terminationSequence = "\x00"
+var (
+	rconServerAddress  = os.Getenv("RCON_SERVER_ADDRESS")
+	rconServerPassword = os.Getenv("RCON_SERVER_PASSWORD")
+)
 
 func main() {
-	conn, err := net.Dial("tcp", "127.0.0.1:27015")
+	r, err := rcon.Dial(rconServerAddress)
 	if err != nil {
 		panic(err)
 	}
-	defer conn.Close()
+	defer r.Close()
 
-	packet := NewPacket(SERVERDATA_AUTH, "hello")
-
-	err = writePacket(&conn, packet)
-	if err != nil {
-		panic(err)
-	}
-	packet, err = readPacket(&conn)
+	err = r.Authenticate(rconServerPassword)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("%+v\n", packet)
-}
+	reader := bufio.NewReader(os.Stdin)
 
-func writePacket(conn *net.Conn, packet *Packet) error {
-	buffer := packet.ToBuffer()
+	for {
+		fmt.Print("Enter text: ")
+		text, _ := reader.ReadString('\n')
+		trimmedText := strings.TrimSpace(text)
 
-	bytes := buffer.Bytes()
-
-	n, err := (*conn).Write(bytes)
-	if n != len(bytes) {
-		return errors.New("Invalid read")
-	}
-	return err
-}
-
-func readPacket(conn *net.Conn) (response *Packet, err error) {
-	var packet Packet
-	if err = binary.Read(*conn, binary.LittleEndian, &packet.Size); err != nil {
-		return
-	}
-	if err = binary.Read(*conn, binary.LittleEndian, &packet.Id); err != nil {
-		return
-	}
-	if err = binary.Read(*conn, binary.LittleEndian, &packet.Type); err != nil {
-		return
-	}
-
-	var n int
-	bytesRead := 0
-	bytesTotal := int(packet.Size - 4)
-	body := make([]byte, bytesTotal)
-
-	for bytesTotal > bytesRead {
-		n, err = (*conn).Read(body[bytesRead:])
+		response, err := r.Execute(trimmedText)
 		if err != nil {
-			return
+			panic(err)
 		}
-		bytesRead += n
+
+		fmt.Printf("Response: %+v\n", response.Body)
 	}
-
-	packet.Body = strings.TrimRight(string(body), terminationSequence)
-
-	return &packet, nil
 }
